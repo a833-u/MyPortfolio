3150bca4ad1f0e8d6d09109f846edeb7fb1d4e12
a833-u <akansara833@gmail.com>
2025-04-18 23:22:21 +0530

    star color changed

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -8,31 +8,31 @@ const Marquee = () => {
       <div className="marquee-inner">
         <ul className="marquee-content">
           <li><MarqueeItem text="UI Design" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar'/>
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar'/>
           <li><MarqueeItem text="Responsive" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Components" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Animations" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Performance" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Accessibility" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           </ul>
         <ul className="marquee-content" aria-hidden="true">
           <li><MarqueeItem text="UI Design" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Responsive" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Components" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Animations" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Performance" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           <li><MarqueeItem text="Accessibility" /></li>
-          <FaRegStar size={50} color='#b5ff6d' className='marqueeStar' />
+          <FaRegStar size={50} color='#898888b4' className='marqueeStar' />
           </ul>
       </div>
       <div className="marquee-fade-left"></div>
